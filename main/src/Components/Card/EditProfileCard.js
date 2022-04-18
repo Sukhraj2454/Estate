d3058bb731560f86688e3af29e31766b20a93bb4
Anshul <kumaranshul8439@gmail.com>
2022-04-18 13:36:47 +0530

    profiledetails

---
+++ main/src/Components/Card/EditProfileCard.js
@@ -0,0 +1,82 @@
+// React Utils
+import { useEffect, useState } from "react";
+
+//MUI Components
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import EditIcon from '@mui/icons-material/Edit';
+import IconButton from '@mui/material/IconButton';
+
+
+
+export default function FormDialog() {
+  const [open, setOpen] = useState(false);
+  const [name, setname] = useState("");
+  const [emailid, setemailid] = useState("");
+  const [contact, setcontact] = useState("");
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    
+    setOpen(false);
+  };
+
+  return (
+    <div>
+
+      <IconButton color="primary" aria-label="EditIcon" onClick={handleClickOpen} >
+        {/* <AddShoppingCartIcon /> */}
+        <EditIcon/>
+      </IconButton>
+   
+     
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle >Edit Profile</DialogTitle>
+        <DialogContent>
+       
+          <TextField onChange={setname}
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Name"
+            type="email"
+            fullWidth
+            variant="standard"
+          />
+           <TextField onChange={setemailid}
+            autoFocus
+            margin="dense"
+            id="email"
+            label="Category"
+            type="email"
+            fullWidth
+            variant="standard"
+          />
+           <TextField
+           onChange={setcontact}
+            autoFocus
+            margin="dense"
+            id="contact"
+            label="Contact"
+            type="email"
+            fullWidth
+            variant="standard"
+          />
+          
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}variant="contained"  >Cancel</Button>
+          <Button onClick={handleClose}variant="contained" >Submit</Button>
+        </DialogActions>
+      </Dialog>
+
+    </div>
+  );
+}
--- main/src/Routes/Worker.js
+++ main/src/Routes/Worker.js
@@ -7,19 +7,27 @@ import { useLocation, Link } from 'react-router-dom';
 import { getUserTasks, getUser, logout } from '../Utils/controller';
 import useWidth from "../Utils/useWidth";
 import MyTasks from '../Components/Dashboard/MyTasks';
+import EditProfileCard from '../Components/Card/EditProfileCard';
 
 // MUI Compoenents
 import { Box, Grid, Typography, Button, IconButton } from "@mui/material";
 import { ThemeProvider } from '@mui/material/styles';
 import Toolbar from '@mui/material/Toolbar';
 import AppBar from '@mui/material/AppBar';
+import Rating from '@mui/material/Rating';
 // import LoadingButton from '@mui/lab/LoadingButton';
 import { createTheme } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+
 
 // MUI Icons
 import HomeIcon from '@mui/icons-material/Home';
 
 export default function Worker({ workers }) {
+
+    //rating 
+    const [value, setValue] = useState(4);
+
     const theme = createTheme();
     const location = useLocation();
     const worker = location.state?.worker;
@@ -71,16 +79,46 @@ export default function Worker({ workers }) {
 
             <Box component='div' sx={{ mt: 1 }} >
                 <Grid container height={620} sx={{ m: 0, p: 0 }}>
-                    <Grid item xs={5}>
+                    <Grid item xs={len}lg={5}>
                         {/* <Avatar sx={{ margin: 0 }} {...stringAvatar('Sukhraj Singh')} /> */}
-                        <Typography textAlign={'center'} component='h1' variant='h4'>Name: {user.name}</Typography>
+                  
+                        <Box xs={len}
+                         sx={{height:300 ,margin:3 }}
+                        >
+                          <Paper elevation={1}sx={{ minHeight: 300 ,minWidth:200,}}      
+                        >
+                           <Grid center container direction={"row"} >
+                               <Grid item >
+                               <Typography textAlign={'center'} component='h1' variant='h4'>{user.name}</Typography>
+                               </Grid>
+                               <Grid item>
+                               <EditProfileCard></EditProfileCard>
+                               </Grid>
+                           </Grid>
+                            {/* <Typography textAlign={'center'} component='h1' variant='h4'>{user.name}</Typography> */}
+                            <Typography textAlign={'center'} component='h1' variant='h5'>{user.contact}</Typography>
+                            </Paper>
+                        </Box>
+                        {/* other details */}
+                        <Box  xs={len}
+                         sx={{height:300 ,margin:3 }
+                        }
+                        >
+                          <Paper elevation={2}sx={{  minHeight: 300 ,minWidth:200,}}  >
+                       <div>
+                       <Typography textAlign={'left'} component='h2' variant='h5' marginLeft={5} marginBottom={1}>Full Name : {user.name}</Typography>
+                   <Typography textAlign={'left'} component='h2' variant='h5'marginLeft={5}marginBottom={1}marginTop={1}>Email: {user.email} </Typography>
+                        <Typography textAlign={'left'} component='h3' variant='h5'marginLeft={5} marginBottom={1}marginTop={1}>Contact: {user.contact}</Typography>
+                        <Typography textAlign={'left'} component='h4' variant='h5'marginLeft={5}marginBottom={1}marginTop={1}>Category: {user.category}</Typography>
+                        <Typography textAlign={'left'} component='h5' variant='h5'marginLeft={5} marginBottom={1}marginTop={1}>Ratings: <Rating name="read-only" value={value} readOnly /> </Typography>
+                       </div>
+                   </Paper>
+                        </Box>
                     </Grid>
-                    <Grid item xs={len} sx={{ border: '2px solid gray', borderBottomRightRadius: 15, borderTopRightRadius: 15 }}>
+                    <Grid item xs={len}  sx={{ border: '2px solid gray', borderBottomRightRadius: 15, borderTopRightRadius: 15 }}>
                         <Typography textAlign={'center'} sx={{ mb: 5 }} component='h1' variant='h4'>
                             Tasks
                         </Typography>
-
-
                         {/* <ACInput
                             label='Status'
                             defValue='To Do'
